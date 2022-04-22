bc7b8d9669634b3b9ad292abfdb6712d5dea7bc2
Sandeep Perumalla <sperumalla@salesforce.com>
2022-04-22 21:10:39 +0530

    General Code Refactoring

--- src/main/java/com/leetcode/solutions/KthLargestElementInAnArray.java
+++ src/main/java/com/leetcode/solutions/KthLargestElementInAnArray.java
@@ -23,7 +23,7 @@ public class KthLargestElementInAnArray {
 
     public static void main(String[] args) {
         KthLargestElementInAnArray kthLargestElementInAnArray = new KthLargestElementInAnArray();
-        System.out.println(kthLargestElementInAnArray.findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));;
-        System.out.println(kthLargestElementInAnArray.findKthLargest(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));;
+        System.out.println(kthLargestElementInAnArray.findKthLargest(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+        System.out.println(kthLargestElementInAnArray.findKthLargest(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));
     }
 }
